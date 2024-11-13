805cf0372ee036df6c98f04ccd3a260e1448338c
shalis <natashaalis089@gmail.com>
2024-11-13 15:43:47 +0800

    Modify Asset Fields

--- assets/models.py
+++ assets/models.py
@@ -8,10 +8,24 @@ class Vendor(models.Model):
     
 
 class Asset(models.Model):
+
+    NOT_AVAILABLE = 'NA'
+    AVAILABLE = 'AV'
+    IN_USE = 'IU'
+
+    STATUS_CHOICES = {
+        NOT_AVAILABLE: 'Not Available',
+        AVAILABLE: 'Available',
+        IN_USE: 'In Use',
+    }
+
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     model = models.CharField(max_length=100)
     borrowed_date = models.DateField(blank=True, null=True)
     returned_date = models.DateField(blank=True, null=True)
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, blank=True, null=True
+    )
 
     def __str__(self):
         return f'{self.vendor} _ {self.model}'
