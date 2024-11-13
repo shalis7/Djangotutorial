b595264413f585a825a137e91d415e7499e5423f
shalis <natashaalis089@gmail.com>
2024-11-13 15:28:31 +0800

    Create Asset model and registed in admin

--- assets/admin.py
+++ assets/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, Asset
 
 admin.site.register(Vendor)
+
+admin.site.register(Asset)
--- assets/models.py
+++ assets/models.py
@@ -5,3 +5,18 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Asset(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    model = models.CharField(max_length=100)
+    borrowed_date = models.DateField(blank=True, null=True)
+    returned_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.vendor} _ {self.model}'
+
+    
+
+class Handover:
+    pass
